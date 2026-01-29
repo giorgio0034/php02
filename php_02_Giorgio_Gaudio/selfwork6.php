3ec66ad120dd48093f55a2f078199843a0c12712
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2026-01-29 20:18:49 +0100

    Add files via upload

---
+++ php_02_Giorgio_Gaudio/selfwork6.php
@@ -0,0 +1,39 @@
+<?php
+
+
+for ($i = 1; $i <= 100; $i++) {
+
+    if($i%3==0 && $i%5==0){
+        echo " hackademy \n";
+       
+
+ } else if ($i % 3 == 0) {
+     echo " php \n" ;
+       
+    }else{
+
+   if ($i % 5 == 0) {
+        echo  "JAVASCRIPT \n";
+        
+     }else{
+        echo $i ."\n";
+     }
+    
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
