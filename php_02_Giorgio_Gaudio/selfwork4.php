0375dd5136d42c58049c2ad467c950964f78393b
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2026-01-29 15:48:30 +0100

    Add files via upload

---
+++ php_02_Giorgio_Gaudio/selfwork4.php
@@ -0,0 +1,27 @@
+<?php
+
+$users = [
+  ['name' => 'Davide', 
+  'surname' => 'Cariola', 
+  'gender' => 'NB'],
+];
+
+
+
+
+
+echo "Buongiorno sign ". $users[0]['name']. " ". $users[0]['surname'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
